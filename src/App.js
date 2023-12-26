e138313b1060a927e0fcd0ca46ab01d27707ebd4
Robert Hendricks <rh12@email.sc.edu>
2023-12-26 17:29:16 -0500

    update app.js to render only JASS

--- src/App.js
+++ src/App.js
@@ -1,69 +1,11 @@
-/* import React from "react";
-import { JaaSMeeting } from "@jitsi/react-sdk";
-
-const VideoConference = () => {
-  const roomName = "my-room";
-
-  return (
-    <JaaSMeeting roomName={roomName} />
-  );
-};
-
-const App = () => {
-  return (
-    <VideoConference />
-  );
-};
-
-export default App; */
+// Jisi As A Service using react sdk
 
 import { JaaSMeeting } from '@jitsi/react-sdk';
 import React, { useState } from 'react';
 
 const App = () => {
-    //const apiRef = useRef();
-    //const [ logItems, updateLog ] = useState([]);
+    
     const [ showNew ] = useState(false);
-    //const [ knockingParticipants, updateKnockingParticipants ] = useState([]);
-/* 
-    const printEventOutput = payload => {
-        updateLog(items => [ ...items, JSON.stringify(payload) ]);
-    };
- */
-   /*  const handleAudioStatusChange = (payload, feature) => {
-        if (payload.muted) {
-            updateLog(items => [ ...items, `${feature} off` ])
-        } else {
-            updateLog(items => [ ...items, `${feature} on` ])
-        }
-    };
-
-    const handleChatUpdates = payload => {
-        if (payload.isOpen || !payload.unreadCount) {
-            return;
-        }
-        apiRef.current.executeCommand('toggleChat');
-        updateLog(items => [ ...items, `you have ${payload.unreadCount} unread messages` ])
-    }; */
-
-    /* const handleKnockingParticipant = payload => {
-        updateLog(items => [ ...items, JSON.stringify(payload) ]);
-        updateKnockingParticipants(participants => [ ...participants, payload?.participant ])
-    };
- */
-    /* const resolveKnockingParticipants = condition => {
-        knockingParticipants.forEach(participant => {
-            apiRef.current.executeCommand('answerKnockingParticipant', participant?.id, condition(participant));
-            updateKnockingParticipants(participants => participants.filter(item => item.id === participant.id));
-        });
-    };
- */
-    /* const handleJitsiIFrameRef1 = iframeRef => {
-        iframeRef.style.border = '10px solid #3d3d3d';
-        iframeRef.style.background = '#3d3d3d';
-        iframeRef.style.height = '400px';
-        iframeRef.style.marginBottom = '20px';
-    }; */
 
     const handleJitsiIFrameRef2 = iframeRef => {
         iframeRef.style.marginTop = '10px';
@@ -79,25 +21,8 @@ const App = () => {
         iframeRef.style.marginBottom = '20px';
     };
 
-    //const handleApiReady = apiObj => {
-        //apiRef.current = apiObj;
-       // apiRef.current.on('knockingParticipant', handleKnockingParticipant);
-        //apiRef.current.on('audioMuteStatusChanged', payload => handleAudioStatusChange(payload, 'audio'));
-       // apiRef.current.on('videoMuteStatusChanged', payload => handleAudioStatusChange(payload, 'video'));
-        //apiRef.current.on('raiseHandUpdated', printEventOutput);
-        //apiRef.current.on('titleViewChanged', printEventOutput);
-       // apiRef.current.on('chatUpdated', handleChatUpdates);
-       // apiRef.current.on('knockingParticipant', handleKnockingParticipant);
-   // };
 
-    //const handleReadyToClose = () => {
-        /* eslint-disable-next-line no-alert */
-       // alert('Ready to close...');
-    //};
-
-    //const generateRoomName = () => `CelloRomm${Math.random() * 100}-${Date.now()}`;
-
-    // Multiple instances demo
+    // return instance
     const renderNewInstance = () => {
         if (!showNew) {
             return null;
@@ -110,29 +35,7 @@ const App = () => {
         );
     };
 
-   /* 
-    const renderLog = () => logItems.map(
-        (item, index) => (
-            <div
-                style = {{
-                    fontFamily: 'monospace',
-                    padding: '5px'
-                }}
-                key = { index }>
-                {item}
-            </div>
-        )
-    );
- */
-   /*  const renderSpinner = () => (
-        <div style = {{
-            fontFamily: 'sans-serif',
-            textAlign: 'center'
-        }}>
-            Loading..
-        </div>
-    );
- */
+   
 
     return (
         <>
@@ -142,17 +45,7 @@ const App = () => {
             }}>
                 Jacqueline Taylor & Friends
             </h1>
-           {/*  <JitsiMeeting
-                roomName = { generateRoomName() }
-                spinner = { renderSpinner }
-                configOverwrite = {{
-                    subject: 'lalalala',
-                    hideConferenceSubject: false
-                }}
-                lang = ''
-                onApiReady = { externalApi => handleApiReady(externalApi) }
-                onReadyToClose = { handleReadyToClose }
-                getIFrameRef = { handleJitsiIFrameRef1 } /> */}
+         {/* render a meeting with Jitsi */}
             <JaaSMeeting
                 appId = { 'vpaas-magic-cookie-d4ea19dabd194f1e803df5dac4ada499' }
                 roomName = { 'CelloRoom' }
@@ -165,9 +58,9 @@ const App = () => {
 
                // useStaging = { true }
                 getIFrameRef = { handleJaaSIFrameRef } />
-            {/* {renderButtons()} */}
+        
             {renderNewInstance()}
-            {/* {renderLog()} */}
+         
         </>
     );
 };
